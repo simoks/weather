2e37bd9e9163d42cb70bea3a4b5ba766c6d4ff99
Mohamed Kouissi <mohamed.kouissi@gmail.com>
2015-08-14 07:50:41 +0100

    Initial Commit

---
+++ weather.forecast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weather.forecast
+//
+//  Created by kouissi mohamed on 14/08/15.
+//  Copyright (c) 2015 kouissi mohamed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weather.forecast/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  weather.forecast
+//
+//  Created by kouissi mohamed on 14/08/15.
+//  Copyright (c) 2015 kouissi mohamed. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
